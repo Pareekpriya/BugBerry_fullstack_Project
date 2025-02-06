1f760c203b85780e653282eb17ed3586649aa19d
Pareekpriya <priyapareek754@gmail.com>
2025-02-06 17:49:29 +0530

    make responsible for mobile screen

--- script/nav.js
+++ script/nav.js
@@ -19,3 +19,9 @@ function changePlaceholder(){
 setInterval(changePlaceholder,1000);
 
 
+document.getElementById("menu-dropdown").addEventListener("change", function() {
+    let selectedValue = this.value;
+    if (selectedValue) {
+        window.location.href = selectedValue; 
+    }
+});
